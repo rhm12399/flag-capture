5845229f04d04c2e651ce954ede21369bd65a466
Rick Mercer <mercer@cs.arizona.edu>
2014-03-24 06:17:40 -0700

    Tests passing now

--- src/tests/SpritesTest.java
+++ src/tests/SpritesTest.java
@@ -29,8 +29,8 @@ public class SpritesTest {
 
     assertFalse(uA.isTagged());
     assertFalse(uA.isInJail());
-    //    assertEquals(0, uA.challengesInvolvedIn());
-    assertEquals(-2, uA.challengesInvolvedIn());
+    assertEquals(0, uA.challengesInvolvedIn());
+    // assertEquals(-2, uA.challengesInvolvedIn());
 
     assertEquals(0, uA.challengesWon());
     assertEquals(0, uA.numberOfRewards());
